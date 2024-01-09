2906e9baff8bbe512d27a3fc7de3506232f59dd7
Aakarsh Singh <aakarsh.singh@oracle.com>
2024-01-09 13:18:22 +0530

    Update GitIgnore

--- src/test/java/dev/aakarsh/movie/PickerViewTest.java
+++ src/test/java/dev/aakarsh/movie/PickerViewTest.java
@@ -4,10 +4,6 @@ import org.json.JSONException;
 
 import java.lang.reflect.InvocationTargetException;
 
-/**
- * Created by AS046134 on 14-05-2016.
- * Seasonal Intern, Cerner Corp
- */
 public class PickerViewTest
 {
     public static void main(String[] args) throws InterruptedException, JSONException, InvocationTargetException
